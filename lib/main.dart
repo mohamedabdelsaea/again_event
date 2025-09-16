b45afd4596dd9002099b2248dff6bb24b6e92f48
m7md-abdelsame3 <mmdabdo94@gmail.com>
2025-09-16 10:38:08 +0300

    * create Ui app

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,10 @@ import 'package:again_evently/core/routes/app_routes.dart';
 import 'package:again_evently/core/routes/page_route_name.dart';
 import 'package:again_evently/core/theme/app_color.dart';
 import 'package:again_evently/modules/provider/setting_provider.dart';
-import 'package:again_evently/modules/splash/splashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'firebase_options.dart';
-
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
