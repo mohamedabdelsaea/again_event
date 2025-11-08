977074b647dcda977a7d9862a2b4d7a62b89099b
mohamed_abdo <mmdabdo94@gmail.com>
2025-11-08 12:41:55 +0200

    # wait google map

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,34 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get home => 'home';
+
+  @override
+  String get map => 'map';
+
+  @override
+  String get favourite => 'favourite';
+
+  @override
+  String get profile => 'profile';
+
+  @override
+  String get addEvent => 'addEvent';
+
+  @override
+  String get language => 'language';
+
+  @override
+  String get theme => 'theme';
+
+  @override
+  String get logout => 'logout';
+}
